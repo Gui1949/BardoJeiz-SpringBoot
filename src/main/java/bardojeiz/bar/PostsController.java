38bccbf2b15c955993e06b9ade60c8a91439df70
Gui1949 <guilhermesantiago12@gmail.com>
2025-09-21 15:31:15 -0300

    feat: old endpoint request

--- src/main/java/bardojeiz/bar/PostsController.java
+++ src/main/java/bardojeiz/bar/PostsController.java
@@ -7,8 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//Swagger
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.io.IOException;
+import java.net.URI;
+//Para fazer web request
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 import bardojeiz.bar.model.Feed;
 
@@ -35,5 +44,33 @@ public class PostsController {
         Feed newFeed = new Feed(nome, descr);
         Feed save = feedRepository.save(newFeed);
         return save;
+    }
+
+    @GetMapping(path = "/old_data", produces = "application/json")
+        @Operation(summary = "Dados do antigo BackEnd", description = "Dados do antigo endpoint em NodeJS do Bar do Jeiz")
+    public String getOldPosts(){
+        
+        HttpClient client = HttpClient.newHttpClient();
+
+                // Cria a requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://bardo-jeiz-server.vercel.app/data"))
+                .header("Accept", "application/json") // Adiciona o cabeçalho Accept
+                .GET()
+                .build();
+
+        System.out.println(request);
+
+        try {
+            // Envia a requisição e recebe a resposta
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+        
     }
 }
